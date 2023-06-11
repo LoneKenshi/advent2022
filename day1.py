6624fda70dc20ea3dbdbc9b5d266b2a3a51fc495
LoneKenshi <92586538+LoneKenshi@users.noreply.github.com>
2023-06-11 16:22:58 +0200

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -1,16 +1,20 @@
 file = open("input.txt", "r")
-highest_sum = 0
+highest_sum = [0, 0, 0]
 current_sum = 0
 
 for line in file:
     if line.isspace():
-        if current_sum > highest_sum:
-            highest_sum = current_sum
+        # for index, elem in enumerate(highest_sum):
+        #     if current_sum > elem:
+        #         highest_sum[index] = current_sum
+        #         break
+        highest_sum.append(current_sum)
+        highest_sum.sort(reverse=True)
+        highest_sum.pop(3)
         current_sum = 0
         continue
+    # print(f"{line}")
     current_sum += int(line)
 
-print(highest_sum)
-
+print(f"{highest_sum[0] + highest_sum[1] + highest_sum[2]}")
 file.close()
-aa
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-if current_sum > highest_sum:
-            highest_sum = current_sum
-            current_sum = 0
